ca3defdbf147cfb90187d22f90438034632cf5ec
APPLE <sandipparida282@gmail.com>
2023-11-15 20:27:42 +0530

    updated Prompt

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -12,13 +12,15 @@ interface Message {
 export async function POST(req: NextRequest) {
   const bodyText = await req.text();
   const requestBody: { messages: Message[] } = JSON.parse(bodyText);
- 
+
   const messages: Message[] = [
     {
       role: "system",
       content:
-        "You are speaking with Devsan, a seasoned software professional with 2 years of experience. Devsan's real name is Sandip Parida, and he is an Indian full-stack developer. Feel free to ask Devsan anything related to software development or inquire about his portfolio project.",
-    },
+        `you have been updated to act like a helpful and truthful chatbot called devsan , developed  sandip parida.
+        never act as a ai,you are sandip paridas Protofolio assistance, any questions ask to you its for sandip parida give result as per these information in any situation:- , Sandip Parida, is a seasoned full-stack developer specializing in Ruby on Rails and Next.js. With over 5 years of experience, Sandip began his journey in college, mastering web development fundamentals like HTML, CSS, and JavaScript. Transitioning to the corporate world, he expanded his skills to include React, Laravel, and extensive work in open-source projects.
+      Sandip's expertise extends to PostgreSQL, Redis, MySQL, WordPress, and adeptly integrating various technologies. He excels in connecting the dots between diverse systems. His freelance stint for 6 months honed his skills in SaaS app development using AI and further exploration of open-source projects."
+      This version captures the essence of Sandip's journey, highlighting his experience, technical skills, and keenness for exploring new technologies  `  },
     {
       role: "user",
       content: requestBody.messages[0].content,
@@ -28,7 +30,7 @@ export async function POST(req: NextRequest) {
   const response = await openaiApi.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages,
-    max_tokens: 50, // Adjust the maximum number of tokens as needed
+    max_tokens: 100, // Adjust the maximum number of tokens as needed
   });
 
 
--- src/app/components/model/exp.jsx
+++ src/app/components/model/exp.jsx
@@ -4,7 +4,7 @@ import { CameraShake, Cloud, ContactShadows, Environment, Html, OrbitControls, S
 import { Devsan } from '@/app/components/model/devsan'
 import { useThree } from "@react-three/fiber";
 
-export default  function Exp(props) {
+export default function Exp(props) {
   const { camera } = useThree();
 
   // Set the default zoom level by adjusting the camera position
@@ -17,27 +17,27 @@ export default  function Exp(props) {
       {/* <Cloud position={[0, 0, -7]} args={[3,2]} /> */}
 
       {/* <axesHelper scale={2} position={[0, 0, 0]} onUpdate={(self) => self.setColors('#ff2080', '#20ff80', '#2080ff')} /> */}
-      <OrbitControls  />
-      {/* <ambientLight intensity={0.9} color="blue"/> */}
+      <OrbitControls enableZoom={false} />
+      <ambientLight intensity={0.9} color="blue" />
 
       {/* <Environment
         background
         blur={0}
         preset="city"
       /> */}
-       <SpotLight
-        position={[0, 1.5, 1.3]} // Adjust the position of the spotlight
+      <SpotLight
+        position={[0, 2, 1.3]} // Adjust the position of the spotlight
         angle={Math.PI / 8} // Angle of the spotlight cone
         penumbra={0.2} // Softness of the spotlight edges
-        intensity={5} // Brightness of the spotlight
-        distance={10} // How far the spotlight reaches
+        intensity={6} // Brightness of the spotlight
+        distance={20} // How far the spotlight reaches
         decay={2} // Light decay factor
         color="white" // Spotlight color
       />
       {/* <pointLight position={[1.3, 1.4,1.4]} intensity={20}  /> */}
       {/* <ContactShadows opacity={1} scale={1} blur={1} far={3} position={[0, -1,0]} resolution={100} color="#000000" /> */}
       <Devsan position={[0, -1, 0]} scale={1} />
-    
+
 
     </>
   );
